1d648409c77b01c96c191b0f7fb1d5779e0cec6f
areski <areski@gmail.com>
2014-07-16 23:42:46 +0200

    update demo view

--- mywebsite/myapp/views.py
+++ mywebsite/myapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
 
 # Create your views here.
+def home(request):
+    now = datetime.datetime.now()
+    html = "<html><body><h1>Welcome to Django-Petit Demo</h1><br /><br />It's now %s</body></html>" % now
+    return HttpResponse(html)
